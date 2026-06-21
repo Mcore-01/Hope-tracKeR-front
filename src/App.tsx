8619e2ed16ec45561a48357237b740d5b1c2cf77
Mcore-01 <dassd2123@gmail.com>
2026-06-21 13:39:51 +0500

    Initial commit with React + TS + Tailwind + MUI

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <div className="p-10">
+      <Button variant="contained" className="bg-blue-500 hover:bg-blue-700">
+        hope
+      </Button>
+    </div>
+  )
+}
+
+export default App
